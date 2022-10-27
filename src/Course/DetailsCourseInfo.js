944e041731a089326c31a16877271b996095aed6
ikbal-mondal <kbalmondal062@gmail.com>
2022-10-27 20:03:26 +0530

    added pdf download

--- src/Course/DetailsCourseInfo.js
+++ src/Course/DetailsCourseInfo.js
@@ -1,7 +1,7 @@
 import { Link, useLoaderData } from "react-router-dom";
 
 import React from "react";
-import ReactDOM from "react-dom";
+
 import Pdf from "react-to-pdf";
 
 const DetailsCourseInfo = () => {
@@ -10,24 +10,17 @@ const DetailsCourseInfo = () => {
   const { title, image_url, details, days, Course_Fees, category_id } = course;
   return (
     <div className="container mx-auto">
+      
+       <div className="flex justify-end my-5">
+       <Pdf targetRef={ref} x={-150} filename="code-example.pdf" >
+        {({ toPdf }) => <button className="btn-secondary btn  "   onClick={toPdf}>Generate Pdf</button>}
+      </Pdf>
+       
+       </div>
       <section class="bg-white dark:bg-gray-900">
         <div class="container px-6 py-10 mx-auto">
-          <div className="flex justify-between">
-            <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
-             {title}
-            </h1>
-           
-            <Pdf targetRef={ref} filename="code-example.pdf">
-        {({ toPdf }) => <button className="btn-secondary btn " onClick={toPdf}>Generate Pdf</button>}
-      </Pdf>
-      <div ref={ref}>
-        <h1>{title}</h1>
-       
-       <p>{details}</p>
-      </div>
-
-          </div>
-          <div class="mt-8 lg:-mx-6 lg:flex lg:items-center">
+          
+          <div ref={ref} class="mt-8 lg:-mx-6 lg:flex lg:items-center">
             <img
               class="object-cover w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-96"
               src={image_url}
@@ -35,6 +28,7 @@ const DetailsCourseInfo = () => {
             />
 
             <div class="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6 ">
+             <h1 className="text-4xl">{title}</h1>
               <p class="text-sm text-blue-500 uppercase">{days} days</p>
 
               <p
@@ -48,8 +42,10 @@ const DetailsCourseInfo = () => {
                 {details}
               </h2>
 
-              <div class="flex items-center mt-6">
-                <div class="mx-4">
+             
+            </div>
+          </div>
+          <div class="mx-4 justify-end flex my-3">
                   <Link to={`/checkOut/${category_id}`}>
                  
                     <button className="btn btn-outline btn-secondary mx-3">
@@ -57,9 +53,6 @@ const DetailsCourseInfo = () => {
                     </button>
                   </Link>
                 </div>
-              </div>
-            </div>
-          </div>
         </div>
       </section>
     </div>
